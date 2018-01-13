101b0fcee5fa70cfa8c6b0029c4b2b16bda62570
Your Name <sul2102@barnard.edu>
2018-01-13 11:31:54 -0500

    trying to fix sizing problem

--- index.js
+++ index.js
@@ -15,7 +15,7 @@ $(document).ready(function() {
 	$('h2').on('click', function() {
 	  $(this).toggleClass('extra-cool-text');
 	});
-	$('p').on('mouseover', function() {
+	$('yummy').on('mouseover', function() {
 	  $(this).animate({ "font-size" : "40px" }, 400)
 	  .animate({ "letter-spacing" : "10px" }, 1000);
 	});
